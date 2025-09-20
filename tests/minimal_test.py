1a55c005597279c9dc7a6844c5daed70a17b72a7
match@raman <match@Raman>
2025-09-21 01:06:17 +1000

    testing:minimal test

--- tests/minimal_test.py
+++ tests/minimal_test.py
@@ -2,11 +2,34 @@ from tucsenspec.tucsencam import TucsenCamera
 from tucsenspec.simulated_camera import DummyInterface
 import numpy as np
 
-def test_camera_operations():
+def test_camera_simulated():
     try:
         camera = TucsenCamera(DummyInterface(), simulate=True)
         camera.initialise()
         camera.set_exposure_time(0.2)
+        print("Eexposure success")
+        camera.get_temperature()
+        print("Get temperature success")
+        camera.get_fan_speed()
+        print("Get fan speed success")
+        frame = camera.grab_frame_safe()
+        assert frame is not None
+        assert type(frame) == np.ndarray
+        assert frame.ndim == 2  
+
+    finally:
+        camera.close_camera()
+
+def test_camera_operations():
+    try:
+        camera = TucsenCamera(DummyInterface())
+        camera.initialise()
+        camera.set_exposure_time(0.2)
+        print("Eexposure success")
+        camera.get_temperature()
+        print("Get temperature success")
+        camera.get_fan_speed()
+        print("Get fan speed success")
         frame = camera.grab_frame_safe()
         assert frame is not None
         assert type(frame) == np.ndarray
